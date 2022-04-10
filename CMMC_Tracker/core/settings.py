6e115cb54bccba11d137ca0d4f6b1439f57cd703
Caden Charlton <82974345+charltonc88@users.noreply.github.com>
2022-04-10 16:30:53 -0600

    Update settings.py

--- CMMC_Tracker/core/settings.py
+++ CMMC_Tracker/core/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_1122')
 DEBUG = config('DEBUG', default=True, cast=bool)
 
 # load production server from .env
-ALLOWED_HOSTS = [164.92.68.77', 'localhost', '127.0.0.1', config('SERVER', default='127.0.0.1')]
+ALLOWED_HOSTS = ['164.92.68.77', 'localhost', '127.0.0.1', config('SERVER', default='127.0.0.1')]
 
 # Application definition
 
